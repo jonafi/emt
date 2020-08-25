0ad33a1c292e50d2015de7bf331bd52492f4de56
unknown <jonafi@gmail.com>
2020-08-25 14:40:46 -0500

    added non-secure info to database component

--- client/src/components/Database.js
+++ client/src/components/Database.js
@@ -3,28 +3,30 @@ import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
 
 function Database() {
-  const { isAuthenticated } = useAuth0(); 
+  const { isAuthenticated } = useAuth0();
   const [data, setData] = useState([]);
- 
+
   useEffect(async () => {
     const result = await axios('/api/all');
-     setData(result.data);
-  },[]);
- 
+    setData(result.data);
+  }, []);
+
   return (
+    <div>
+      <h1>DB info below</h1>
+      {isAuthenticated && (
+        <ul>
+          {data.map(item => (
+            <li key={item.employee_id}>
+              <h2>{item.info}</h2>
+            </li>
+          ))}
+        </ul>
+      )
+          }
+</div>
 
-    
-    isAuthenticated && (
-
-    <ul>
-      {data.map(item => (
-        <li key={item.employee_id}>
-          <h2>{item.info}</h2>
-        </li>
-      ))}
-    </ul>
-  )
   );
 }
- 
+
 export default Database;
