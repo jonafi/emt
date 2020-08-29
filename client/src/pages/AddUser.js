4641ab6cc90cee13a6866c3a6362cab36306452f
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-08-28 20:52:52 -0500

    created adduser.js

--- client/src/pages/AddUser.js
+++ client/src/pages/AddUser.js
@@ -1,11 +1,37 @@
 import React from 'react';
-
+import {Container, Row, Col, Form, Button} from 'react-bootstrap'
 
 
 
 function AddUser(){
     return (
-        <>AddUser</>
+        <>
+        <Container className="AddUser">
+            <Row>
+                <Col>
+                    <Form>
+                        <Form.Group controlId ="add">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control type = "email" placeholder="name@example.com"></Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId ="add">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control type = "text" placeholder="First Name"></Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId ="add">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control type = "text" placeholder="Last Name"></Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId ="add">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control type = "text" placeholder="123 Street"></Form.Control>
+                        </Form.Group>
+                        <Button as="input" type="submit" value="Submit" />{' '}
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+        </>
     );
 };
 
