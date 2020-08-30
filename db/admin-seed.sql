6589e71b6a119f204a9809b22db650e1da39ea74
unknown <jonafi@gmail.com>
2020-08-29 21:20:04 -0500

    added 5 admin emails to seed data

---
+++ db/admin-seed.sql
@@ -0,0 +1,10 @@
+insert into employees(first_name, last_name, employees.role, department, status, primary_phone, personal_email, hire_date, birth_date)
+values ("john", "ferro", "admin", "IT","","","jonafi@gmail.com",0, 0);
+insert into employees(first_name, last_name, employees.role, department, status, primary_phone, personal_email, hire_date, birth_date)
+values ("tim", "tanner", "admin", "IT","","","timtanner@tanner-companies.com",0, 0);
+insert into employees(first_name, last_name, employees.role, department, status, primary_phone, personal_email, hire_date, birth_date)
+values ("christian", "gonzalez", "admin", "IT","","","christian.gonzalez.2633@gmail.com",0, 0);
+insert into employees(first_name, last_name, employees.role, department, status, primary_phone, personal_email, hire_date, birth_date)
+values ("william", "sorn", "admin", "IT","","","sornwilliam@gmail.com",0, 0);
+insert into employees(first_name, last_name, employees.role, department, status, primary_phone, personal_email, hire_date, birth_date)
+values ("vanida ", "somchaleunsouk", "admin", "IT","","","vsomsouk@gmail.com",0, 0);
