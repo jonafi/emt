d5aa5a909ee4f9cad09119e819d6292a219ca801
J Ferro <jonafi@gmail.com>
2020-09-07 13:28:41 -0500

    nav bar links conditional, only logged in users can see

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,23 +5,27 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { Navbar } from 'react-bootstrap';
 
 function Nav() {
-  const { isLoading } = useAuth0();
+  const { isLoading, isAuthenticated} = useAuth0();
   if (isLoading) return <div>...</div>; //prevents seeing wrong button
 
   return (
     <>
       <Navbar className="navbar">
         <Navbar.Brand href="/Dashboard" className="Hero ltgray">EMPLOYEE HERO</Navbar.Brand>
-        <Navbar.Brand href="/Directory" className="ltgray">DIRECTORY</Navbar.Brand>
-        <Navbar.Brand href="/Performance" className="ltgray">PERFORMANCE</Navbar.Brand>
-        <Navbar.Brand href="/AddUser" className="ltgray">ADD USER</Navbar.Brand>
-          <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-            </Navbar.Collapse>
-            <LoginButton/>
-            <LogoutButton/>
+        { isAuthenticated &&
+        <>
+          <Navbar.Brand href="/Directory" className="ltgray">DIRECTORY</Navbar.Brand>
+          <Navbar.Brand href="/Performance" className="ltgray">PERFORMANCE</Navbar.Brand>
+          <Navbar.Brand href="/AddUser" className="ltgray">ADD USER</Navbar.Brand>
+</>
+}
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+        </Navbar.Collapse>
+        <LoginButton />
+        <LogoutButton />
       </Navbar>
-      
+
     </>
   );
 }
