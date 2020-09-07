8a9af88838073db2c2ddd79e83e0ff02d74fa082
Soul <sornwilliam@gmail.com>
2020-09-07 18:28:39 -0500

    Changes to Dashboard.js(It now filters correctly). Changes to Navbar.js(Add User only to admins). Other minor changes and slight styling.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import LoginButton from './LoginButton'
 import LogoutButton from './LogoutButton';
 import { useAuth0 } from '@auth0/auth0-react'
 import { Navbar } from 'react-bootstrap';
+import API from '../utils/API';
 
 function Nav() {
   const { isLoading } = useAuth0();
   if (isLoading) return <div>...</div>; //prevents seeing wrong button
+
+  const { user, isAuthenticated } = useAuth0();
+  const [data, setData] = useState([]);
+  const [role, setRole] = useState([]);
+
+  useEffect(() => {
+    loadEmployees();
+  }, []);
+
+  useEffect(() => {
+    loadRole();
+  }, []);
+
+  function loadEmployees() {
+    API.getEmployees()
+      .then(result => {
+        setData(result.data);
+      })
+      .catch(err => console.log(err));
+  }
+
+  function loadRole(user) {
+    API.getUser(user)
+      .then(result => {
+        setRole(result.data.role);
+        console.log(result.data.role)
+      })
+      .catch(err => console.log(err));
+  }
+
 
   return (
     <>
@@ -14,7 +45,17 @@ function Nav() {
         <Navbar.Brand href="/Dashboard" className="Hero ltgray">EMPLOYEE HERO</Navbar.Brand>
         <Navbar.Brand href="/Directory" className="ltgray">DIRECTORY</Navbar.Brand>
         <Navbar.Brand href="/Performance" className="ltgray">PERFORMANCE</Navbar.Brand>
-        <Navbar.Brand href="/AddUser" className="ltgray">ADD USER</Navbar.Brand>
+        
+        {isAuthenticated && (
+          <>
+            {loadRole(user.email)}
+            {(role === 'admin')
+              ? <Navbar.Brand href="/AddUser" className="ltgray">ADD USER</Navbar.Brand>
+              : <p></p>
+            }
+          </>
+        )}
+
           <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
             </Navbar.Collapse>
