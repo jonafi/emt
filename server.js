5931f013bb0f711d599b3104902092431aa3cc9b
J Ferro <jonafi@gmail.com>
2020-09-10 21:12:52 -0500

    auth take 3 typo

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // middleware allows only authenticated users to see API routes
 const authCheck = (req,res,next)=>{
   console.log('\x1b[36m%s\x1b[0m', req.headers.referer)
-  if(req.headers.referer===("http://localhost:3000/"||"https://team-c2c-emt/")){
+  if(req.headers.referer===("http://localhost:3000/"||"https://team-c2c-emt.herokuapp.com/")){
     next()
   }
   else if(req.headers.cookie){
