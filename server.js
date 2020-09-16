4d70af6b6b78910b0934e16a907ad49126e89f88
Christian Gonzalez <christian.gonzalez.2633@gmail.com>
2020-09-15 20:26:44 -0500

    server changes

--- server.js
+++ server.js
@@ -122,10 +122,10 @@ const io = socketIO(server);
 // Heroku won't actually allow us to use WebSockets
 // so we have to setup polling instead.
 // https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
+// io.configure(function () { 
+//   io.set("transports", ["xhr-polling"]); 
+//   io.set("polling duration", 10); 
+// });
 
 io.on("connection", (socket) => {
   console.log("New client connected");
