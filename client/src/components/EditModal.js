19373646657f24ec80f41f9a2b7552a3037a0913
Christian Gonzalez <christian.gonzalez.2633@gmail.com>
2020-09-17 19:12:25 -0500

    index password for me

--- client/src/components/EditModal.js
+++ client/src/components/EditModal.js
@@ -60,6 +60,24 @@ function EditModal() {
     const [birthDate, updateBirthDate] = useState("");
     const [hireDate, updateHireDate] = useState("");
 
+    const [data, useData] = useState({
+      "address_line1": "",
+      "address_line2": "",
+      "city": "",
+      "state": "",
+      "zip": 0,
+      "primary_phone": 0,
+      "personal_email": user.email,
+    })
+
+    function handleChange(evt) {
+      const value = evt.target.value;
+      setState({
+        ...state,
+        [evt.target.name]: value
+      });
+    }
+
     //
     function handleOnSubmit (event) {
         event.preventDefault();
@@ -113,28 +131,28 @@ function EditModal() {
 
                       <Form.Group controlId="add">
                         <Form.Label>Address Line 1</Form.Label>
-                        <Form.Control type="text" defaultValue={filteredPerson.address_line1} onChange={e => updateAddress(e.target.value)} />
+                        <Form.Control type="text" name="address_line1" defaultValue={filteredPerson.address_line1} value={data.address_line1} onChange={handleChange} />
                       </Form.Group>
 
                       <Form.Group controlId="add">
                         <Form.Label>Address Line 2</Form.Label>
-                        <Form.Control type="text" defaultValue={filteredPerson.address_line2} onChange={e => updateAddressTwo(e.target.value)} />
+                        <Form.Control type="text" name="address_line2" defaultValue={filteredPerson.address_line2} value={data.address_line2} onChange={handleChange} />
                       </Form.Group>
 
                       <Form.Row>
                             <Form.Group as={Col} controlId="add">
                                 <Form.Label>City</Form.Label>
-                            <Form.Control defaultValue={filteredPerson.city} onChange={e => updateCity(e.target.value)}/>
+                            <Form.Control name="city" defaultValue={filteredPerson.city} value={data.city} onChange={handleChange}/>
                             </Form.Group>
 
                                 <Form.Group as={Col} controlId="add">
                                     <Form.Label>State</Form.Label>
-                                    <Form.Control defaultValue={filteredPerson.state} onChange={e => updateUSState(e.target.value)} />
+                                    <Form.Control name="state" defaultValue={filteredPerson.state} value={data.state} onChange={handleChange} />
                                 </Form.Group>
 
                                 <Form.Group as={Col} controlId="add">
                                     <Form.Label>Zip</Form.Label>
-                                    <Form.Control defaultValue={filteredPerson.zip} onChange={e => updateZip(e.target.value)} />
+                                    <Form.Control name="zip" defaultValue={filteredPerson.zip} value={data.zip} onChange={handleChange} />
                                 </Form.Group>
                       </Form.Row>
 
